f5dc0c5565974cbd1be509ad08813ad2e6f19090
Matheus Fiebig <macrubig@gmail.com>
2024-04-25 02:58:30 -0300

    added some plugins, fixed some keys

--- nvim/lua/f/plugins/ui.lua
+++ nvim/lua/f/plugins/ui.lua
@@ -1,8 +1,5 @@
 return {
   'nvim-tree/nvim-web-devicons',
   'DaikyXendo/nvim-material-icon',
-  'matveyt/neoclip',
   'nvim-lualine/lualine.nvim',
-  'smoka7/hop.nvim',
-  'm4xshen/autoclose.nvim'
 }
