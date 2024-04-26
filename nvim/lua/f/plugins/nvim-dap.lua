eff9ba96c2e598ebd9af5e6568418505b4f868e1
Matheus Fiebig <macrubig@gmail.com>
2024-04-26 03:15:08 -0300

    configured ui

--- nvim/lua/f/plugins/nvim-dap.lua
+++ nvim/lua/f/plugins/nvim-dap.lua
@@ -16,8 +16,16 @@ return {
         name = "attach - netcoredbg",
         request = "attach",
         processId = require('dap.utils').pick_process,
-        program = function()
-          return vim.fn.input('', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        --[[program = function()
+          return vim.fn.input('', vim.fn.getcwd() .. '/bin/Debug/net8.0/docker-api-test.dll', 'file')
+        end]]
+      },
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function() --To launch a project update the line bellow or add a dap-per-project
+          return vim.fn.input('', vim.fn.getcwd() .. '/bin/Debug/net8.0/docker-api-test.dll', 'file')
         end
       }
     }
