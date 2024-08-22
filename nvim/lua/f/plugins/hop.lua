4c0ddc14166ab69777dea58ab8b75d6f4ea24104
Matheus Fiebig <macrubig@gmail.com>
2024-08-22 13:43:28 -0300

    lots of themes

--- nvim/lua/f/plugins/hop.lua
+++ nvim/lua/f/plugins/hop.lua
@@ -4,10 +4,4 @@ return {
         require('lightspeed').setup({});
     end
 }
---return {
---'smoka7/hop.nvim',
---config = function()
---require('hop').setup()
---end
---}
---
+
--- nvim/lua/f/plugins/ui.lua
+++ nvim/lua/f/plugins/ui.lua
@@ -1,5 +1,42 @@
 return {
     'nvim-tree/nvim-web-devicons',
     'DaikyXendo/nvim-material-icon',
-    'nvim-lualine/lualine.nvim',
+    {
+        'nvim-lualine/lualine.nvim',
+        config = function()
+            require('lualine').setup({
+                options = { theme = require 'lualine.themes.dracula' }
+            })
+        end
+    },
+    --{
+    --'neanias/everforest-nvim',
+    --config = function()
+    --vim.cmd("colorscheme everforest")
+    --end,
+    --}
+    {
+        'bgwdotdev/gleam-theme-nvim',
+        config = function()
+            vim.cmd("colorscheme gleam")
+        end,
+    },
+    --{
+        --'ribru17/bamboo.nvim',
+        --config = function()
+            --vim.cmd("colorscheme bamboo")
+        --end,
+    --}
+    --{
+        --'ricardoraposo/gruvbox-minor.nvim',
+        --config = function()
+            --vim.cmd("colorscheme gruvbox-minor")
+        --end,
+    --}
+    --{
+        --'luisiacc/handmade-hero-theme',
+        --config = function()
+            --vim.cmd("colorscheme handmade-hero-theme")
+        --end,
+    --}
 }
