b7ad1a4009b0f47f360cd8b5549a0008d857f7f6
Matheus Fiebig <macrubig@gmail.com>
2025-02-17 02:57:28 -0300

    breakpoint

--- nvim/lua/f/plugins/nvim-dap.lua
+++ nvim/lua/f/plugins/nvim-dap.lua
@@ -1,13 +1,31 @@
+local function define_colors()
+    vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#b91c1c" })
+    vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bold = true })
+
+    vim.fn.sign_define("DapBreakpoint", {
+        text = "🔴",
+        numhl = "DapBreakpoint",
+    })
+    vim.fn.sign_define("DapStopped", {
+        text = "🟢",
+        linehl = "DapStopped",
+        numhl = "DapStopped",
+    })
+end
+
 vim.g.dotnet_build_project = function()
     local default_path = vim.fn.getcwd() .. '/'
+
     if vim.g['dotnet_last_proj_path'] ~= nil then
         default_path = vim.g['dotnet_last_proj_path']
     end
+
     local path = vim.fn.input('Path to your *proj file', default_path, 'file')
     vim.g['dotnet_last_proj_path'] = path
+
     local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
-    print('')
-    print('Cmd to execute: ' .. cmd)
+    print('\nCmd to execute: ' .. cmd)
+
     local f = os.execute(cmd)
     if f == 0 then
         print('\nBuild: ✔️ ')
@@ -17,7 +35,7 @@ vim.g.dotnet_build_project = function()
 end
 
 vim.g.dotnet_dll = function()
-    local default_path = vim.fn.getcwd() .. '/'
+    local default_path = vim.fn.getcwd() .. '/bin/Debug/'
     if vim.g['dotnet_last_dll'] ~= nil then
         default_path = vim.g['dotnet_last_dll']
     end
@@ -26,42 +44,41 @@ vim.g.dotnet_dll = function()
 end
 
 return {
-  'mfussenegger/nvim-dap',
-  config = function()
-    local dap = require('dap')
-    local debuggers_folder = vim.fn.stdpath('data') .. "/mason"
+    'mfussenegger/nvim-dap',
+    config = function()
+        local dap = require('dap')
+        local debuggers_folder = vim.fn.stdpath('data') .. "/mason"
 
-    dap.adapters.coreclr = {
-      type = "executable",
-      command = debuggers_folder .. "/packages/netcoredbg/netcoredbg",
-      args = { "--interpreter=vscode" }
-    }
+        define_colors()
 
-    dap.configurations.cs = {
-      {
-        type = "coreclr",
-        name = "attach - netcoredbg",
-        request = "attach",
-        processId = require('dap.utils').pick_process,
-      },
-      {
-        type = "coreclr",
-        name = "launch - netcoredbg",
-        request = "launch",
-        cwd = '${workspaceFolder}',
-        env = {
-          ASPNETCORE_ENVIRONMENT = function()
-            return "Development"
-          end,
-          --ASPNETCORE_URLS = function()
-            --return "https://localhost:80"
-          --end,
-        },
-        program = function() --To launch a project update the line bellow or add a dap-per-project
-          vim.g.dotnet_build_project()
-          return vim.g.dotnet_dll();
-        end
-      }
-    }
-  end,
+        dap.adapters.coreclr = {
+            type = "executable",
+            command = debuggers_folder .. "/packages/netcoredbg/netcoredbg",
+            args = { "--interpreter=vscode" }
+        }
+
+        dap.configurations.cs = {
+            {
+                type = "coreclr",
+                name = "attach - netcoredbg",
+                request = "attach",
+                processId = require('dap.utils').pick_process,
+            },
+            {
+                type = "coreclr",
+                name = "launch - netcoredbg",
+                request = "launch",
+                cwd = '${workspaceFolder}',
+                env = {
+                    ASPNETCORE_ENVIRONMENT = function()
+                        return "Development"
+                    end,
+                },
+                program = function()
+                    vim.g.dotnet_build_project()
+                    return vim.g.dotnet_dll();
+                end
+            }
+        }
+    end,
 }
