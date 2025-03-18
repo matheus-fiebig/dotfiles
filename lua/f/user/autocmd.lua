6ff23f2526eb1a13d9d9b1a3f07783985b37d2b5
Matheus Fiebig <macrubig@gmail.com>
2025-03-18 02:03:07 -0300

    yank color

--- lua/f/user/autocmd.lua
+++ lua/f/user/autocmd.lua
@@ -1,8 +1,9 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-	pattern = "*",
-	desc = "Highlight selection on yank",
-	callback = function()
-		vim.highlight.on_yank({ timeout = 200, visual = true })
-	end,
+    group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+    pattern = "*",
+    desc = "Highlight selection on yank",
+    callback = function()
+        vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#c8ffa7", fg = "#292d3e" })
+        vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 200, visual = true })
+    end,
 })
