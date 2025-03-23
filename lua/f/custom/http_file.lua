3f6d53fe4631768b05e0508b9c7780db7d3c4b54
Matheus Fiebig <macrubig@gmail.com>
2025-03-22 22:34:04 -0300

    feat: http

--- lua/f/custom/http_file.lua
+++ lua/f/custom/http_file.lua
@@ -2,6 +2,8 @@ local table_utils = require("utils.table")
 local parser = require("utils.parser")
 local json_formatter = require("utils.json_formatter")
 
+local variables = {}
+
 ---map an item to http header
 ---@param item table
 ---@return string
@@ -16,46 +18,51 @@ local function can_use_header(item)
     return not item.disabled and item.key and item.value
 end
 
+---@param tbl table
+---@param key string
+---@param value table
+---@return string
+local function create_http_template_parsed(tbl, key, value)
+    if key == "request" then
+        if value.url and value.method then
+            local body = ""
+            if value.body then
+                body = vim.json.decode(value.body.raw)
+            end
+
+            local name = "Request " .. tostring(tbl)
+            if tbl.name then
+                name = tbl.name
+            end
+
+            local data_to_parse = {
+                name = name,                                                                               --optional
+                method = value.method,                                                                     --required
+                url = value.url.raw,                                                                       --required
+                headers = table_utils.concat_to_string(value.header, map_to_string, can_use_header, "\n"), --optional
+                body = json_formatter:pretty_print(body)                                                   --optional
+            }
+
+            local template = "# {{name}}\n{{method}} {{url}}\n {{headers}}\n\n{{body}}\n\n\n"
+            return parser.parse_template(template, data_to_parse)
+        end
+    end
+end
+
 ---iterate through the itens recursively and append to file
 ---@param tbl table
 ---@param file file*
+---@param callback function
 ---@return nil
-local function iterate_through_item(tbl, file)
+local function iterate_through_item(tbl, file, callback)
     for key, value in pairs(tbl) do
         if key == "item" then
             for idx, _ in ipairs(value) do
-                iterate_through_item(value[idx], file)
+                iterate_through_item(value[idx], file, callback)
             end
         end
 
-        if key == "request" then
-            if value.url and value.method then
-                local body = ""
-                if value.body then
-                    body = vim.json.decode(value.body.raw)
-                end
-
-                local name = "Request " .. tostring(tbl)
-                if tbl.name then
-                    name = tbl.name
-                end
-
-                local data_to_parse = {
-                    name = name,                                                                               --optional
-                    method = value.method,                                                                     --required
-                    url = value.url.raw,                                                                       --required
-                    headers = table_utils.concat_to_string(value.header, map_to_string, can_use_header, "\n"), --optional
-                    body = json_formatter:pretty_print(body)                                                   --optional
-                }
-
-                local template = "# {{name}}\n{{method}} {{url}}\n {{headers}}\n\n{{body}}\n\n\n"
-                --should not write the file here
-                file:write(parser.parse_template(template, data_to_parse))
-            end
-        end
-
-        if key == "variable" then
-        end
+        file:write(callback(tbl, key, value))
     end
 end
 
@@ -65,11 +72,11 @@ end
 ---@return nil
 function To_http(path, filename)
     local file = io.open(path .. filename .. ".json", "r")
-    local working_file = io.open(path .. filename .. ".http", "r+")
+    local working_file = io.open(path .. filename .. ".http", "w+")
 
     if working_file and file then
         local file_as_json = vim.json.decode(file:read("*a"))
-        iterate_through_item(file_as_json, working_file)
+        iterate_through_item(file_as_json, working_file, create_http_template_parsed)
         working_file:close();
         file:close();
 
