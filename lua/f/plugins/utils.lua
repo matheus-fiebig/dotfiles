37a2d0103400eafd2ffc237b926b71ecdec97e8b
Matheus Fiebig <macrubig@gmail.com>
2025-04-03 01:06:40 -0300

    feat: removed aerial keymap

--- lua/f/plugins/utils.lua
+++ lua/f/plugins/utils.lua
@@ -16,14 +16,7 @@ return {
     {
         "stevearc/aerial.nvim",
         config = function()
-            require("aerial").setup({
-                -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-                on_attach = function(bufnr)
-                    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-                    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-                end,
-            })
+            require("aerial").setup({})
         end
     }
 }
-
